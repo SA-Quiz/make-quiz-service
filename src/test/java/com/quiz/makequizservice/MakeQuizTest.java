e7764a293b22c375ab39844c9a2d559a21fd894a
Esselink, Cas C <c.esselink@student.fontys.nl>
2024-04-09 09:46:53 +0200

    first test

---
+++ src/test/java/com/quiz/makequizservice/MakeQuizTest.java
@@ -0,0 +1,14 @@
+package com.quiz.makequizservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.quiz.makequizservice.MakeQuizController;
+
+@SpringBootTest
+public class MakeQuizTest {
+    @Test
+    void makeQuiz() {
+        MakeQuizController makeQuizController = new MakeQuizController();
+        assert makeQuizController.makequiz().equals("Make a quiz");
+    }
+}
